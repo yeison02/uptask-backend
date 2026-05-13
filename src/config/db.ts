0131435c0bf5452fa36191efddcbcd87180494ef
Yeison 02 <yeison.suarezt@gmail.com>
2026-05-13 10:44:49 -0500

    Añadiendo la conexión a MongoBD

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import colors from 'colors';
+import { exit } from 'node:process';
+
+export const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.DATABASE_URL as string);
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+        console.log(colors.magenta.bold(`MongoDB conectado en: ${url}`));
+
+    } catch (error) {
+        console.log(colors.red.bold('Error al conectar a MongoDB'));    
+        exit(1);
+    }
+}
